4b1bdea17c863bd509d0ef6844e93a5c8c77043c
Velvetvibes69 <amayasunil11@gmail.com>
2026-05-10 19:31:19 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+i= int(input("Enter a Number:"))
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in If block")
+else:
+    print("number is greater than 15")
+    print("i'm in Else block")
+print("i'm not in if and not in Else block")
